9820a8f2fa9d63236b406c7f3ae775714f120572
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-05-08 10:07:15 +0100

    Updates

---
+++ precache-manifest.5c89a8e282bf183debe35dd30e9babf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc8746b492603c0eec0ab4405028030",
+    "url": "/Creadit-card/index.html"
+  },
+  {
+    "revision": "9b09cac0fbd646c51456",
+    "url": "/Creadit-card/static/css/main.db2e72a9.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/Creadit-card/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "9b09cac0fbd646c51456",
+    "url": "/Creadit-card/static/js/main.9044106d.chunk.js"
+  },
+  {
+    "revision": "d2291ac79019ef828c9a",
+    "url": "/Creadit-card/static/js/runtime~main.f45eda44.js"
+  },
+  {
+    "revision": "20c0f4062c99bfbf16c4519e3371bc16",
+    "url": "/Creadit-card/static/media/background.20c0f406.jpg"
+  },
+  {
+    "revision": "a8c8030be44fdafe49518bd5325953a4",
+    "url": "/Creadit-card/static/media/pus.a8c8030b.png"
+  },
+  {
+    "revision": "007dcb603e915afd331ec2dc01661f94",
+    "url": "/Creadit-card/static/media/visa.007dcb60.png"
+  }
+]);
